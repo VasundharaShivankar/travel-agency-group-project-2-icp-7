b696c0308ecf59fc74fd2bd00945d5402686380b
VasundharaShivankar <vasundharashivankar179@gmail.com>
2024-04-12 23:21:38 +0530

    likes increment decrement

--- js/blog_and_travel_tips.js
+++ js/blog_and_travel_tips.js
@@ -1,42 +1,31 @@
-// Get the like button, dislike button, and like count element
-const likeBtn = document.getElementById('like-btn');
-const dislikeBtn = document.getElementById('dislike-btn');
-const likeCount = document.getElementById('like-count');
-
 let likes = 0;
-
-// Function to increment likes
 function incrementLikes() {
+    const likeBtn = document.getElementById('like-btn');
+    const likeCount = document.getElementById('like-count');
   likes++;
   likeCount.textContent = likes;
 }
-
-// Function to decrement likes
 function decrementLikes() {
+    const dislikeBtn = document.getElementById('dislike-btn');
+    const likeCount = document.getElementById('like-count');
   if (likes > 0) {
     likes--;
     likeCount.textContent = likes;
   }
 }
-
-// Event listeners for like button clicks
 likeBtn.addEventListener('click', incrementLikes);
 dislikeBtn.addEventListener('click', decrementLikes);
 
 
-function isValidEmail() {
-    var emailInput = document.getElementById("emailInput").value.trim();
-    var errorMessage = document.getElementById("errorMessage");
-
-    // Regular expression for email validation
-    var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    
+function isValidEmail() { 
     if (emailPattern.test(emailInput)) {
-        // Valid email format
+        var emailInput = document.getElementById("emailInput").value.trim();
+        var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
         errorMessage.textContent = "";
         return true;
     } else {
-        // Invalid email format
+        var errorMessage = document.getElementById("errorMessage");
+        var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
         errorMessage.textContent = "Please enter a valid email address in the format 'example@example.com'";
         alert("Invalid email format. Please correct it.");
         return false;
